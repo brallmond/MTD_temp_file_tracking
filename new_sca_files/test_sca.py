002af1a5b1eb64c20aed74db960719c4630b1378
root <root@Legolas200.teststand>
2022-12-08 10:25:49 -0600

    moved from commenting in or out single functions to using a list for a set of functions to test

--- new_sca_files/test_sca.py
+++ new_sca_files/test_sca.py
@@ -6,14 +6,13 @@ def test_on(sca_chip):
   sca_chip.send_connect()
   test_read_ID(sca_chip)
   sca_chip.write_control_reg("CRB", randint(1,255))
-  sca_chip.read_control_reg("CRB")
+  return sca_chip.read_control_reg("CRB")
 
 def test_read_ID(sca_chip):
   # tested, works
   sca_chip.send_connect()
   sca_chip.enable_ADC()
-  sca_chip.read_ID()
-
+  return sca_chip.read_ID()
 
 def test_read_write_three_registers(sca_chip):
   # tested (once), works
@@ -23,70 +22,85 @@ def test_read_write_three_registers(sca_chip):
   sca_chip.write_control_reg("CRC", randint(1,255))
   sca_chip.read_control_reg("CRC")
   sca_chip.write_control_reg("CRD", randint(1,255))
-  sca_chip.read_control_reg("CRD")
+  return sca_chip.read_control_reg("CRD")
  
 
 def test_read_and_reset_SEU(sca_chip):
   # tested, works (only ever read zero in the SEU counter)
+  # broken!
   sca_chip.send_connect()
   sca_chip.read_SEU() 
-  sca_chip.reset_SEU() 
+  return sca_chip.reset_SEU() 
 
 
 def test_I2C_enable(sca_chip):
   # tested, works
   sca_chip.send_connect()
   for i in range(1, 16):
-    sca_chip.enable_I2C_channel(i)
+    if i == 15:
+      return sca_chip.enable_I2C_channel(i)
+    else:
+      sca_chip.enable_I2C_channel(i)
 
 
-def test_make_errors(sca_chip):
+def test_make_command_error(sca_chip):
   # tested, works
-  print(Indicators.INFO + 
-        "verify the chip throws no errors for a normal operation" + 
-        Indicators.RESET)
-  test_on(sca_chip)
-  print(Indicators.WARNING + 
-        "now purposely making errors" + 
-        Indicators.RESET)
-  sca_chip.make_command_error()
-  sca_chip.make_channel_error()
+  print("make error 0x4 = 0b0000 0100!")
+  # supply command to read SCA ID for V1 of the chip
+  reg = SCA_Register.CTRL_R_ID.value
+  return sca_chip.send_command_passthrough(reg.Channel, reg.Length, 0x91, reg.Data)
 
 
-def test_read_errors(sca_chip):
+def test_make_channel_error(sca_chip):
   # tested, works
-  sca_chip.send_connect()
-  error = sca_chip.make_channel_error()
-  sca_chip.check_error(error)
-  error = sca_chip.make_command_error()
-  sca_chip.check_error(error)
-  print("demonstrate no error")
-  no_error = sca_chip.enable_I2C_channel(1)
-  sca_chip.check_error(no_error)
+  print("make error 0x20 = 0b0010 0000!")
+  # supply channel to 0x13 to ID CMD instead of 0x14
+  reg = SCA_Register.CTRL_R_ID.value
+  return sca_chip.send_command_passthrough(0x13, reg.Length, reg.CMD, reg.Data)
+
+
+def test_make_length_error(sca_chip):
+  print("make error 0x10 = 0b0100 0000!")
+  reg = SCA_Register.CTRL_R_ID.value
+  return sca_chip.send_command_passthrough(0x13, reg.Length, reg.CMD, reg.Data)
 
 
 def test_GPIO_read_write(sca_chip):
   # tested (once), works
   sca_chip.send_connect()
   sca_chip.write_GPIO(17)
-  sca_chip.read_GPIO()
+  return sca_chip.read_GPIO()
+
 
 if __name__ == "__main__":
 
   chippy = sca_chip()
   
   print("Hi, I'm chippy, your SCA chip! I'm gonna run some tests now...")
-  #test_on(chippy)
-  #test_read_ID(chippy)
-  #test_read_write_three_registers(chippy)
-  #test_read_and_reset_SEU(chippy) #broken!
-  #test_I2C_enable(chippy)
-  #test_make_errors(chippy) # check again after factoring ID
-  #test_read_errors(chippy)
-  #test_GPIO_read_write(chippy)
-  
-  #for test in list_of_tests:
-  #  rxpayload = test
-  #  chippy.check_error(rxpayload)
 
+  list_of_tests = [ 
+    test_on,
+    test_read_ID,
+    test_read_write_three_registers,
+    test_read_and_reset_SEU, #broken!
+    test_I2C_enable,
+    test_make_command_error,
+    test_make_channel_error,
+    test_make_length_error,
+    test_GPIO_read_write,
+  ]
+
+  error_tests = [
+    test_on,
+    test_make_command_error,
+    test_make_channel_error,
+    test_make_length_error,
+  ]
+
+  list_to_test = list_of_tests
+
+  for i,test in enumerate(list_to_test):
+    print(CMDLINECOLOR.INFO + f"########## TEST {i} ##########" + CMDLINECOLOR.RESET)
+    rxpayload = test(chippy)
+    chippy.check_error(rxpayload)
 
